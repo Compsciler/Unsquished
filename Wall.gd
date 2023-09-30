26bd778d8b973230058e69d29f5ef3bc299a2175
Roger Wang <rogerwang2520@gmail.com>
2023-09-29 20:01:29 -0500

    Add wall movement

---
+++ Wall.gd
@@ -0,0 +1,12 @@
+extends AnimatableBody2D
+
+@export var speed = 100
+@onready var normal = $CollisionShape2D.get_shape().normal;
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	position += normal * speed * delta
