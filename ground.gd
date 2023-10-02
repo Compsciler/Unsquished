31b70473dcf64287d03ec6b3c0b9db19952400d6
Revan Ji <jiruifancr@gmail.com>
2023-10-02 02:14:41 -0500

    stage changes

--- ground.gd
+++ ground.gd
@@ -12,6 +12,13 @@ var times = [beat * 0, beat * 80, beat * 144, beat * 256]
 	$Signpost3AudioStreamPlayer,
 ]
 
+@onready var signpost_bgs = [
+	$GrassTilemap,
+	$Clouds,
+	$Space1,
+	$Space2
+]
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -21,6 +28,10 @@ func _ready():
 func _process(delta):
 	if signpost_idx < len(times) and time >= times[signpost_idx]:
 		signpost_audios[signpost_idx].play()
-		# Change tile sprites here
+		for i in range(len(signpost_bgs)):
+			if i == signpost_idx:
+				signpost_bgs[i].show()
+			else:
+				signpost_bgs[i].hide()
 		signpost_idx += 1
 	time += delta
